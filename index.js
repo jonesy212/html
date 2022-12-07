2ca5073c9c97c77b4adde5e595d4e36a939e9516
Marcus Jones <indelibleink84@gmail.com>
2022-12-07 17:33:20 -0600

    updated html

---
+++ index.js
@@ -0,0 +1,251 @@
+
+let titles = ['Web Development Project', 'App Development Project', 'UI Development Project']
+let ids = {
+  web: {},
+  app: {},
+  ui: {},
+}
+// for(title of titles)
+
+// let getTitle = function (title) {
+//   for(title of titles) {
+    
+//   }
+  
+// }
+
+getTitle()
+//  < div class="modal-dialog" >
+{/* <header class="modal-header"> */}
+{/* <h3>UI Project 2</h3> */}
+{/* <i class="fas fa-times" data-close></i> */}
+// </header>
+{/* <div class="modal-body"> */}
+{/* <img class="img-wrapper" src="./assets/images/portfolio-8.jpg" alt="portfolio image"> */}
+{/* <div class="text-wrapper"> */}
+  {/* <p><strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</strong></p> */}
+  {/* <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p> */}
+  {/* <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p> */}
+// </div>
+// </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let courses = [];
+// let modals = [];
+
+// let modal = [{
+
+//     title,
+//     image,
+//     description,
+//     paragraph,
+//   }]
+
+// let ids = {
+//   app :['app-1', 'app-2', 'app-3'],
+//   web :['web-1', 'web-2', 'web-3'],
+//   ui:['ui-1', 'ui-2'],
+// }
+
+// let createModal = modal.map(function (element) {
+//   for (id of ids) {
+//     modal[id].push(element);
+//   }
+//   return element;
+//  })
+
+
+
+// createModal()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // // let html = ["h1", "h2", "h3", "h4", "h5", "div", "section"];
+    
+// // // let list = document.getElementById("html-list");
+
+// // // html.forEach((item) => {
+// // //   let li = document.createElement("li");
+// // //   li.textContent = item;
+// // //   list.appendChild(li);
+// // // });
+
+
+// // // const PortfolioModal = {
+// // //   div,
+// // //   header,
+// // //   h3,
+  
+// // // }
+
+// // let app = 'app';
+// // let web = 'web';
+// // let ui = 'ui';
+// // // let services = [
+// // //   app = [ 'app-1','app-2', 'app-3' ]
+// // // ]
+
+// // // let title = ['Web Development Project','App Development Project', 'UI Designs']
+
+// // // let getTitle = services.forEach(service => {
+// // //   return service
+// // // })
+
+// // let Services = {
+// //   courses:{
+// //     web: {},
+// //     app: {},
+// //     ui: {},
+// //   }
+// // }
+
+
+
+// // let ids = [
+// //   app = ['app-1', 'app-2', 'app-3'],
+// //   web = ['web-1', 'web-2', 'web-3'],
+// //   ui =  ['ui-1', 'ui-2']
+// // ];
+// // let titles = [
+// //   appTitle = 'App Development Project',
+// //   webTitle = 'Web Development Project',
+// //   uiTitle = 'UI Design Project'
+// // ]
+
+// // let matched = 
+// //   //match app/web/ui to the titles
+
+// //   titles.forEach(function (title) {
+  
+// //     console.log(title)
+// //     if (title.includes('Web')) {
+// //       console.log('true',title)
+// //     }
+// //   })
+  
+// //   ids.forEach( id => {
+// //     if (id.includes('web-1')) {
+// //       console.log('true',id)
+// //     }
+// //     console.log(id)
+
+// //   })
+
+
+
+
+// // // let servicesList = services.map(service =>{
+// // //   return service
+// // // })
+
+
+// // // 
+// // // let createModal = new Modal = () => {
+// // //   //create div tag
+// // //   let div = document.createElement("div");
+// // //   let modal = div.setAttribute("modal");
+// // //   console.log(modal)
+// // // }
+// // // console.log('creating modal');
+// // // createModal();
+
+// // //adds text to the div
+// // // card.forEach((element) => {
+// // //   const div = document.createElement("div");
+// // //   div.textContent = element
+// // //   for(const id of )
+// // //   let id = 
+// // //   document.body.appendChild(div);
+
+// //   //add more elements within the div
+// // // })
+
+// // // let 
+// // // div id class="modal" data-animation=
+// // // header class="modal-header"
+// // // h3 title
+// // // i class="fas fa-times" data-close
+// // // div class='modal-body'
+// // // img class='img-wraper'
+// // // div class='text-wrapper'
+// // // p<strong></strong>
+// // // p
+// // // p
+
+
+// // //  < div class="modal-dialog" >
+// // // <header class="modal-header">
+// // // <h3>UI Project 2</h3>
+// // // <i class="fas fa-times" data-close></i>
+// // // </header>
+// // // <div class="modal-body">
+// // // <img class="img-wrapper" src="./assets/images/portfolio-8.jpg" alt="portfolio image">
+// // // <div class="text-wrapper">
+// // //   <p><strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</strong></p>
+// // //   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
+// // //   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
+// // // </div>
+// // // </div>
+
+
+
+// // // var ids = ['id1', 'id2', 'id3'];
+// // // ids.forEach(function(e) {
+// // //   document.body.innerHTML += `
+// // // <div class="post" id=${e}">
+// // //            <blockquote
+// // //                class="instagram-media"
+// // //                data-instgrm-permalink="https://www.instagram.com/p/${e}
+// // //                data-instgrm-version="13">
+// // //           </blockquote>                
+// // //     </div>
+// // // `
+// // // })
